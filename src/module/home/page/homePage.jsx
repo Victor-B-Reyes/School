f822b90c329e1e0b600a95100829f5a13ac4aa60
victor-b-reyes <victor_blanco_reyes@outlook.com>
2026-04-22 15:31:15 -0600

    Nuew logica 3

--- src/module/home/page/homePage.jsx
+++ src/module/home/page/homePage.jsx
@@ -134,13 +134,14 @@ function HomePage(){
                                         </h2>
                                         <div className="space-y-4">
                                             <div>
-                                                <h3 className="font-semibold text-gray-800">Dipl. en Informática</h3>
-                                                <p className="text-sm text-gray-500">Instituto INTEC Hispanoamericano (2024-2025)</p>
-                                            </div>
-                                            <div>
                                                 <h3 className="font-semibold text-gray-800">Tec. en Mantenimiento Industrial</h3>
                                                 <p className="text-sm text-gray-500">CECyTEV No.25 (2016-2019)</p>
                                             </div>
+                                            <div>
+                                                <h3 className="font-semibold text-gray-800">Dipl. en Informática</h3>
+                                                <p className="text-sm text-gray-500">Instituto INTEC Hispanoamericano (2017-2019)</p>
+                                            </div>
+                                            
                                             <div>
                                                 <h3 className="font-semibold text-gray-800">Ing. Sistemas Computacionales</h3>
                                                 <p className="text-sm text-gray-500">Instituto Tecnológico de Tuxtepec (2019-2025)</p>
